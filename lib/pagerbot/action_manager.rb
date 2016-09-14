689e17a855fe7bc3ea56c6893aea5a1c1d716e62
Tido Carriero <tido@segment.com>
2016-09-14 12:15:28 -0700

    update logging

--- lib/pagerbot/action_manager.rb
+++ lib/pagerbot/action_manager.rb
@@ -55,7 +55,7 @@ module PagerBot
     # because who doesn't like one-liners. Feel free to refactor!
     +PagerBot::Utilities::DispatchMethod
     def list_schedules(query, event_data={})
-      PagerBot.log.info v.aliases
+      @pagerduty.schedules.list.map { |v| PagerBot.log.info v.aliases }
       PagerBot.log.info 'TTG'
       msg = "Here are the schedules I know about (schedules in the same line are synonyms):\n" +
         @pagerduty.schedules.list.map { |v| Utilities.pluck('name', v.aliases).sort.join(', ') }.sort.join("\n")
